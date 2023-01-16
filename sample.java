bdc0ae8bd18fe816a827f8772471d9cd346657ee
root <root@ip-172-31-10-185.ap-south-1.compute.internal>
2023-01-16 13:43:07 +0000

    f1

--- sample.java
+++ sample.java
@@ -1,6 +1,8 @@
-class simple {
-public static void main (String args []){
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
-system.out.print1n ("welcome parveen malik ");
-}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
